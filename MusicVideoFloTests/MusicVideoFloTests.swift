a39b8c794f714086d98963c0cd62545a83c829c0
Florian <florian.ludot@gmail.com>
2016-06-25 22:06:44 +0200

    Initial Commit

---
+++ MusicVideoFloTests/MusicVideoFloTests.swift
@@ -0,0 +1,36 @@
+//
+//  MusicVideoFloTests.swift
+//  MusicVideoFloTests
+//
+//  Created by Florian LUDOT on 25/06/16.
+//  Copyright © 2016 Florian LUDOT. All rights reserved.
+//
+
+import XCTest
+@testable import MusicVideoFlo
+
+class MusicVideoFloTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
